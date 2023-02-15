1d830eeb7bbafc9103704b43b9890e59ffbb950c
prasadsai <prasadsai0369@gmail.com>
2023-02-15 16:29:31 -0500

    Given an array and an integer k, return the array-form of the integer

---
+++ Easy/src/ArrayFormOfInteger.java
@@ -0,0 +1,39 @@
+import java.util.List;
+
+public class ArrayFormOfInteger {
+    public List<Integer> addToArrayForm(int[] num, int k) {
+        boolean carry = false;
+        int i = num.length-1;
+        while((k>0 || carry) && i>=0){
+            if(carry){
+                num[i]+=k%10+1;
+                carry = false;
+            }else{
+                num[i]+=k%10;
+            }
+            if(num[i]>9){
+                num[i]%=10;
+                carry = true;
+            }
+            k/=10;
+            i--;
+        }
+
+        List<Integer> li = new ArrayList<>();
+        for(i=0;i<num.length;i++){
+            li.add(num[i]);
+        }
+        while(k>0){
+            if(carry){
+                k+=1;
+                carry = false;
+            }
+            li.add(0,k%10);
+            k/=10;
+        }
+        if(carry){
+            li.add(0,1);
+        }
+        return li;
+    }
+}
