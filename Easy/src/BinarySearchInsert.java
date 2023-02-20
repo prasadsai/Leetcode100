b365d8ced3b2f230435a33f14fc496d6c80e9b14
prasadsai <prasadsai0369@gmail.com>
2023-02-20 10:46:04 -0500

    need to change the file name BinaryTreeZigZac to Zag, and also binarySearchInsert to Binary

--- Easy/src/BinarySearchInsert.java
+++ Easy/src/BinarySearchInsert.java
@@ -1,4 +1,4 @@
-public class binarySearchInsert {
+public class BinarySearchInsert {
     public int searchInsert(int[] nums, int target) {
         int start = 0, end = nums.length - 1;
         int mid;
